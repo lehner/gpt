476659f2abd64d0eb28bdb36f104d866569f6821
Christoph Lehner <christoph@lhnr.de>
2020-02-23 17:09:02 -0500

    cleanup scalar/CG test

--- lib/gpt/default.py
+++ lib/gpt/default.py
@@ -5,11 +5,29 @@
 #
 import sys
 
-def get_vec(tag, default):
+def get(tag,default):
     if tag in sys.argv:
         i = sys.argv.index(tag)
         if i+1 < len(sys.argv):
-            return [ int(x) for x in sys.argv[i+1].split(".") ]
+            return sys.argv[i+1]
     return default
 
-grid = get_vec("--grid",[4,4,4,4])
+def get_float(tag, default = float("nan")):
+    res=get(tag,None)
+    if not res is None:
+        return float(res)
+    return default
+
+def get_int(tag, default):
+    res=get(tag,None)
+    if not res is None:
+        return int(res)
+    return default
+
+def get_ivec(tag, default):
+    res=get(tag,None)
+    if not res is None:
+        return [ int(x) for x in res.split(".") ]
+    return default
+
+grid = get_ivec("--grid",[4,4,4,4])
--- tests/scalar.py
+++ tests/scalar.py
@@ -6,15 +6,18 @@
 #
 import gpt as g
 
+m0crit=4.0
+m0=g.default.get_float("--mass",0.1) + m0crit
+
 grid=g.grid(g.default.grid, g.single)
 
 src=g.complex(grid)
 src[:]=0
 src[0,0,0,0]=1
 
-# Create a free Klein-Gordon operator
+# Create a free Klein-Gordon operator (spectrum from mass^2-16 .. mass^2)
 def A(dst,src,mass):
-    g.eval(dst,mass*src)
+    g.eval(dst,(mass**2.)*src)
     for i in range(4):
         g.eval(dst, dst + g.cshift(src, i, 1) + g.cshift(src, i, -1) - 2*src )
 
@@ -58,9 +61,9 @@ def CG(mat,src,psi,tol,maxit):
 # Perform CG
 psi=g.lattice(src)
 psi[:]=0
-CG(lambda i,o: A(o,i,1.0),src,psi,1e-8,1000)
+CG(lambda i,o: A(o,i,m0),src,psi,1e-8,1000)
 
 # Test CG
-A(tmp,psi,1.0)
+A(tmp,psi,m0)
 g.message("True residuum:", g.norm2(g.eval(tmp - src)))
 
