0fe44e62eae9f839cf917341db544e73e36c8689
Daniel Richtmann <daniel.richtmann@gmail.com>
2020-03-30 16:00:15 +0200

    Add some linear solvers + tests

---
+++ tests/solvers.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+#
+# Authors: Daniel Richtmann 2020
+#
+# Desc.: Exercise linear solvers
+#
+import gpt as g
+import numpy as np
+import sys
+import time
+import os.path
+
+# load configuration
+homedir = os.path.expanduser("~")
+U = g.load(homedir + "/configs/openqcd/test_16x8_pbcn6")
+
+# do everything in single-precision
+U = g.convert(U, g.single)
+
+# use the gauge configuration grid
+grid=U[0].grid
+
+# quark
+w=g.qcd.fermion.wilson_clover(U,{
+    "kappa" : 0.13565,
+    "csw_r" : 2.0171,
+    "csw_t" : 2.0171,
+    "xi_0" : 1,
+    "nu" : 1,
+    "isAnisotropic" : False,
+    "boundary_phases" : [ 1.0, 1.0, 1.0, 1.0 ]
+})
+
+# create point source
+src=g.mspincolor(grid)
+g.create.point(src, [0,0,0,0])
+
+# build solvers
+s = g.qcd.fermion.solver
+slv_cg = s.propagator(
+    s.eo_ne(g.qcd.fermion.preconditioner.eo2(w),
+            g.algorithms.iterative.cg({
+                "eps": 1e-6,
+                "maxiter": 1000
+            })))
+slv_mr = s.propagator(
+    s.eo_ne(
+        g.qcd.fermion.preconditioner.eo2(w),
+        g.algorithms.iterative.mr({
+            "eps": 1e-6,
+            "maxiter": 1000,
+            "relax": 1.0
+        })))
+slv_bicgstab = s.propagator(
+    s.eo_ne(g.qcd.fermion.preconditioner.eo2(w),
+            g.algorithms.iterative.bicgstab({
+                "eps": 1e-6,
+                "maxiter": 1000
+            })))
+slv_fgcr = s.propagator(
+    s.eo_ne(
+        g.qcd.fermion.preconditioner.eo2(w),
+        g.algorithms.iterative.fgcr({
+            "eps": 1e-6,
+            "maxiter": 1000,
+            "restartlen": 20
+        })))
+slv_fgmres = s.propagator(
+    s.eo_ne(
+        g.qcd.fermion.preconditioner.eo2(w),
+        g.algorithms.iterative.fgmres({
+            "eps": 1e-6,
+            "maxiter": 1000,
+            "restartlen": 20
+        })))
+
+# rhs vectors
+dst_cg=g.mspincolor(grid)
+dst_mr=g.mspincolor(grid)
+dst_bicgstab=g.mspincolor(grid)
+dst_fgcr=g.mspincolor(grid)
+dst_fgmres=g.mspincolor(grid)
+
+# perform solves
+slv_cg(src, dst_cg)
+print("CG finished")
+slv_mr(src, dst_mr)
+print("MR finished")
+slv_bicgstab(src, dst_bicgstab)
+print("BICGSTAB finished")
+slv_fgcr(src, dst_fgcr)
+print("FGCR finished")
+slv_fgmres(src, dst_fgmres)
+print("FGMRES finished")
