482df0e45760cacd1392e7148d1c350afc2e81e1
Christoph Lehner <christoph@lhnr.de>
2020-04-20 07:16:19 -0400

    more tests

--- tests/random/random-simple.py
+++ tests/random/random-simple.py
@@ -5,6 +5,7 @@
 # Desc.: Illustrate core concepts and features
 #
 import gpt as g
+import numpy as np
 
 rng=g.random("block_seed_string_13")
 n=10000
@@ -45,4 +46,15 @@ grid=g.grid([4,4,4,4],g.double)
 v=g.complex(grid)
 rng.normal(v)
 
-g.message(v)
+test_sequence_comp=np.array([ v[0,0,0,0].real, v[2,0,0,0].real, v[0,2,0,0].real, v[1,3,1,3].real, v[3,2,1,0].real ])
+test_sequence_ref =np.array([ -1.11423690141998, -0.590684470804368, 1.87872413138069, -0.941747191206284, 0.401453969380243 ],np.float64)
+
+g.message(test_sequence_comp)
+
+err=np.linalg.norm(test_sequence_comp - test_sequence_ref)
+assert(err < 1e-14)
+
+g.message(err)
+
+
+g.message("All tests passed")
