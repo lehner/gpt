34c656b9d366c3bff8be14d3bfa2cfbfb94d54c0
Christoph Lehner <christoph@lhnr.de>
2020-05-06 09:51:00 -0400

    update test

--- tests/qcd/domain-wall.py
+++ tests/qcd/domain-wall.py
@@ -70,7 +70,51 @@ slv_qz(src,dst_qz)
 # two-point
 correlator_qm=g.slice(g.trace(dst_qm*g.adj(dst_qm)),3)
 correlator_qz=g.slice(g.trace(dst_qz*g.adj(dst_qz)),3)
+correlator_ref=[
+    0.5805132985115051,
+    0.2003495693206787,
+    0.05248865857720375,
+    0.02542622946202755,
+    0.015156489796936512,
+    0.009473282843828201,
+    0.005946052260696888,
+    0.003997922874987125,
+    0.0027736490592360497,
+    0.001767154666595161,
+    0.0010931078577414155,
+    0.0007353568216785789,
+    0.0004973784671165049,
+    0.0003497783036436885,
+    0.0002569338248576969,
+    0.00018832855857908726,
+    0.00016109236457850784,
+    0.00017106340965256095,
+    0.00020944568677805364,
+    0.00029460625955834985,
+    0.00045832418254576623,
+    0.000751025159843266,
+    0.001192546333186328,
+    0.001964164897799492,
+    0.003390846075490117,
+    0.005769025068730116,
+    0.008645612746477127,
+    0.0126266498118639,
+    0.0183967035263776,
+    0.0275730341672897,
+    0.0572328567504882,
+    0.2176910340785980
+]
 
 # output
+eps_qm=0.0
+eps_qz=0.0
 for t in range(len(correlator_qm)):
-    g.message(t,correlator_qm[t].real,correlator_qz[t].real)
+    eps_qm+=abs(correlator_qm[t].real - correlator_ref[t])
+    eps_qz+=abs(correlator_qz[t].real - correlator_ref[t])
+    g.message(t,correlator_qm[t].real,correlator_qz[t].real,correlator_ref[t])
+g.message("Test results: %g %g" % (eps_qm,eps_qz))
+assert(eps_qm < 1e-7)
+assert(eps_qz < 1e-3)
+g.message("Test successful")
+
+
