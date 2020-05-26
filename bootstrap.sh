b23fc7b6fec3084be2d904071233c83450054634
Stefan Solbrig <stefan.solbrig@ur.de>
2020-05-27 00:19:15 +0200

    Prepare for "make dist" and "make distcleancheck".

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,15 @@ CGPT_CCFILES = \\' \
 sed -e '$s, \\$,,' \
 > cgpt_ccfiles.inc
 
+# Generate list of *.cc
+echo "Generate list of header files..."
+find lib/cgpt/lib -type f -name '*.h' | \
+sed -e '1i\
+CGPT_HFILES = \\' \
+-e 's,$, \\,' | \
+sed -e '$s, \\$,,' \
+> cgpt_hfiles.inc
+
 # Generate list of python files in lib/gpt
 echo "Generate list of py files..."
 find lib/gpt -type f -name '*.py' | \
