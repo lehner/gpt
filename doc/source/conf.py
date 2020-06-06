5bf5ba7552f791cd6f3e2df4b906621121fe3938
max <max@swk-web.com>
2020-06-06 14:38:36 +0200

    Fix Ipython lexer in strange environments

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'IPython.sphinxext.ipython_console_highlighting',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
