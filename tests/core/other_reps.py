df2aff82a3e73351e85a2ee87690bfa024bd7d24
Christoph Lehner <christoph@lhnr.de>
2020-07-25 17:23:37 +0200

    Update other_reps.py
    WIP: in this test the U fields are not unitary as they are not obtained from random.lie.
    Need to adjust this file.

--- tests/core/other_reps.py
+++ tests/core/other_reps.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #
 # Authors: Tilo Wettig 2020
+#          Christoph Lehner 2020
 #
 # test code for new gauge field types
 #
@@ -13,6 +14,14 @@ L = [8, 4, 4, 4]
 grid_dp = g.grid(L, g.double)
 grid_sp = g.grid(L, g.single)
 
+# unitarity test
+def check_unitarity(U, eps_ref):
+  eye = g.lattice(U)
+  eye[:] = np.eye(U.otype.shape[0], dtype=U.grid.precision.complex_dtype)
+  eps = ( g.norm2(U * g.adj(U) - eye) / g.norm2(eye) ) ** 0.5
+  g.message(f"Test unitarity: {eps}")
+  assert eps < eps_ref
+
 ################################################################################
 # Test SU(2) fundamental and conversion to adjoint
 ################################################################################
@@ -22,20 +31,26 @@ U = g.qcd.gauge.random(grid_sp, rng, otype=g.ot_matrix_su2_fundamental())
 eps = abs(g.qcd.gauge.plaquette(U) - 0.8813162545363108)
 g.message("Test SU(2) fundamental single", eps)
 assert eps < 1e-7
-V = g.qcd.gauge.fund2adj(U)
+check_unitarity(U, 1e-7)
+
+V = g.qcd.gauge.fundamental_to_adjoint(U)
 eps = abs(g.qcd.gauge.plaquette(V) - 0.7126823001437717)
 g.message("Test SU(2) fundamental to adjoint single", eps)
 assert eps < 1e-7
+check_unitarity(V, 1e-7)
 
 rng = g.random("test")
 U = g.qcd.gauge.random(grid_dp, rng, otype=g.ot_matrix_su2_fundamental())
 eps = abs(g.qcd.gauge.plaquette(U) - 0.8813162591343201)
 g.message("Test SU(2) fundamental double", eps)
+check_unitarity(U, 1e-14)
+
 assert eps < 1e-14
-V = g.qcd.gauge.fund2adj(U)
+V = g.qcd.gauge.fundamental_to_adjoint(U)
 eps = abs(g.qcd.gauge.plaquette(V) - 0.7126822868786024)
 g.message("Test SU(2) fundamental to adjoint double", eps)
 assert eps < 1e-14
+check_unitarity(V, 1e-14)
 
 ################################################################################
 # Test SU(2) adjoint
@@ -46,9 +61,12 @@ U = g.qcd.gauge.random(grid_sp, rng, otype=g.ot_matrix_su2_adjoint())
 eps = abs(g.qcd.gauge.plaquette(U) - 0.712682286898295)
 g.message("Test SU(2) adjoint single", eps)
 assert eps < 1e-7
+check_unitarity(U, 1e-7)
 
 rng = g.random("test")
 U = g.qcd.gauge.random(grid_dp, rng, otype=g.ot_matrix_su2_adjoint())
 eps = abs(g.qcd.gauge.plaquette(U) - 0.7126822868786024)
 g.message("Test SU(2) adjoint double", eps)
 assert eps < 1e-14
+check_unitarity(U, 1e-14)
+
