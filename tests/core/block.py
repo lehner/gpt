1c71e31138378c625410d6ca0d86025134255b3c
Daniel Richtmann <daniel.richtmann@gmail.com>
2020-08-11 17:53:38 +0200

    Make block.py central file for testing everything related to blocking

--- tests/core/block.py
+++ tests/core/block.py
@@ -21,6 +21,18 @@ def vc12():
     return g.vcomplex(fine_grid, 12)
 
 
+# masks
+full_mask, blockeven_mask = g.complex(fine_grid), g.complex(fine_grid)
+full_mask[:] = 1
+coor = g.coordinates(blockeven_mask)
+block = np.array(fine_grid.ldimensions) / np.array(coarse_grid.ldimensions)
+block_cb = coor[:, :] // block[:]
+g.make_mask(blockeven_mask, np.sum(block_cb, axis=1) % 2 == 0)
+
+# lookup tables
+full_lut = g.lookup_table(coarse_grid, full_mask)
+blockeven_lut = g.lookup_table(coarse_grid, blockeven_mask)
+
 # basis
 n = 30
 res = None
@@ -50,3 +62,31 @@ for dtype in [vsc, vc12]:
     err2 = g.norm2(lcoarse - lcoarse2) / g.norm2(lcoarse)
     g.message(err2)
     assert err2 < 1e-12
+
+    # more coarse fields, randomize fine field
+    scoarse = g.complex(coarse_grid)
+    mcoarse = g.mcomplex(coarse_grid, n)
+    mcoarse2 = g.lattice(mcoarse)
+    mcoarse[:], mcoarse2[:] = 0.0, 0.0
+    rng.cnormal(tmpf)
+
+    # test lookup table project against full project
+    g.block.project(lcoarse, tmpf, basis)
+    g.block.project_using_lut(lcoarse2, tmpf, basis, full_lut)
+
+    # report error
+    err2 = g.norm2(lcoarse - lcoarse2) / g.norm2(lcoarse)
+    g.message(err2)
+    assert err2 == 0.0
+
+    # test lookup table project against masked inner product (mask with zeros)
+    for j, bv in enumerate(basis):
+        g.block.masked_inner_product(scoarse, blockeven_mask, bv, tmpf)
+        mcoarse[:, :, :, :, j, 0] = scoarse[:]
+    g.block.project_using_lut(lcoarse, tmpf, basis, blockeven_lut)
+    mcoarse2[:, :, :, :, :, 0] = lcoarse[:]
+
+    # report error
+    err2 = g.norm2(mcoarse - mcoarse2) / g.norm2(mcoarse)
+    g.message(err2)
+    assert err2 == 0.0
