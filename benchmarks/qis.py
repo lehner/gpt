2cd51ddcd42d3d0de33dd9daf3f8395f94990194
Christoph Lehner <christoph@lhnr.de>
2020-08-31 11:11:49 -0400

    style

--- benchmarks/qis.py
+++ benchmarks/qis.py
@@ -16,12 +16,14 @@ r = g.random("qis_test")
 n = g.default.get_int("--n", 16)
 N = g.default.get_int("--N", 100)
 
-for precision in [g.single,g.double]:
-    g.message(f"""
+for precision in [g.single, g.double]:
+    g.message(
+        f"""
 
 Run tests with {n} qubits in {precision.__name__} precision
 
-""")
+"""
+    )
     stR = state(r, n, precision=precision)
     stR.randomize()
 
@@ -30,27 +32,33 @@ Run tests with {n} qubits in {precision.__name__} precision
         return R_z(i, np.pi)
 
     for gate in [X, H, Z]:
-        g.message(f"""
- - Benchmark {gate.__name__} gate""")
+        g.message(
+            f"""
+ - Benchmark {gate.__name__} gate"""
+        )
         circuit = gate(0)
-        for j in range(1,N):
+        for j in range(1, N):
             circuit = circuit | gate(j % n)
         t0 = g.time()
         st = circuit * stR
         t1 = g.time()
         GB = st.lattice.global_bytes() * N * 2 / 1e9
-        g.message(f"""
+        g.message(
+            f"""
    Number of applications :   {N}
    Timing                 :   {t1-t0} s
-   Effective bandwidth    :   {GB/(t1-t0)} GB/s""")
+   Effective bandwidth    :   {GB/(t1-t0)} GB/s"""
+        )
 
     # test performance of two-qubit gates
     for gate in [CNOT]:
-        g.message(f"""
- - Benchmark {gate.__name__} gate""")
-        circuit = gate(0,1)
-        for j in range(1,N):
-            idx = j % (n*n)
+        g.message(
+            f"""
+ - Benchmark {gate.__name__} gate"""
+        )
+        circuit = gate(0, 1)
+        for j in range(1, N):
+            idx = j % (n * n)
             control = idx // n
             target = idx % n
             if control == target:
@@ -60,7 +68,9 @@ Run tests with {n} qubits in {precision.__name__} precision
         st = circuit * stR
         t1 = g.time()
         GB = st.lattice.global_bytes() * N * 2 / 1e9
-        g.message(f"""
+        g.message(
+            f"""
    Number of applications :   {N}
    Timing                 :   {t1-t0} s
-   Effective bandwidth    :   {GB/(t1-t0)} GB/s""")
+   Effective bandwidth    :   {GB/(t1-t0)} GB/s"""
+        )
