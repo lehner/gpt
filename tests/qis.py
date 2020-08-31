f39978be143ce1628bb485b9ebe06c357f0f1e20
Christoph Lehner <christoph@lhnr.de>
2020-08-31 05:43:34 -0400

    more style

--- tests/qis.py
+++ tests/qis.py
@@ -8,20 +8,21 @@ import gpt as g
 import numpy as np
 import sys, cgpt
 
+
 def qubit_state(register_size):
-    return g.complex(g.grid([ 2**n for n in register_size ], g.double))
+    return g.complex(g.grid([2 ** n for n in register_size], g.double))
+
 
 # 3,1 state
-state=qubit_state([3,1,1])
+state = qubit_state([3, 1, 1])
 
 # could split in mpi over first register
 
 # initialize to |000,0,0>
-state[:]=0
-state[0,0,0]=1
+state[:] = 0
+state[0, 0, 0] = 1
 
 # apply gates
-#X(0) * state
+# X(0) * state
 
 g.message(state)
-
