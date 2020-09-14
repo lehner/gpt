a77427403cb84733092b4e13a8e6eb0f5430f68d
lorenzo_barca <Lorenzo1.Barca@physik.uni-regensburg.de>
2020-09-14 18:16:13 +0200

    Added test code for Wuppertal smearing routine

--- tests/create/smear.py
+++ tests/create/smear.py
@@ -28,7 +28,23 @@ eps2 = g.norm2(dst1 - dst2) / g.norm2(dst1)
 g.message(f"Covariance test: {eps2}")
 assert eps2 < 1e-29
 
-# Test smearing operator on point source over unit gauge field
+
+# Test covariance of wuppertal smearing operator
+smear = g.create.smear.wuppertal(U, kappa=0.25, steps=3, dimensions=[0, 1, 2])
+U_transformed = g.qcd.gauge.transformed(U, V)
+smear_transformed = g.create.smear.wuppertal(
+    U_transformed, kappa=0.25, steps=3, dimensions=[0, 1, 2]
+)
+
+src = g.mspincolor(grid)
+rng.cnormal(src)
+dst1 = g(V * smear * src)
+dst2 = g(smear_transformed * V * src)
+eps2 = g.norm2(dst1 - dst2) / g.norm2(dst1)
+g.message(f"WUPPERTAL SMEARING - Covariance test: {eps2}")
+assert eps2 < 1e-29
+
+# Test gauss smearing operator on point source over unit gauge field
 for dimensions in [[0, 1, 2], [0, 1, 2, 3]]:
     for sigma, steps in [(0.5, 3), (0.16, 2)]:
         smear_unit = g.create.smear.gauss(
@@ -48,3 +64,26 @@ for dimensions in [[0, 1, 2], [0, 1, 2, 3]]:
         eps2 = g.norm2(dst - factor * src_mom) / g.norm2(dst)
         g.message(f"Gaussian test using eigen representation: {eps2}")
         assert eps2 < 1e-29
+
+
+# Test wuppertal smearing operator on point source over unit gauge field
+for dimensions in [[0, 1, 2], [0, 1, 2, 3]]:
+    for kappa, steps in [(0.25, 3), (0.16, 2)]:
+        smear_unit = g.create.smear.wuppertal(
+            U_unit, kappa=kappa, steps=steps, dimensions=dimensions
+        )
+        src = g.vcolor(grid)
+        src[:] = g.vcolor([1, 0, 0])
+
+        # anti-periodic boundary conditions in time mean space and time are
+        # differently quantized
+        p = 2.0 * np.pi * np.array([1, 2, 3, 4.5]) / L
+        src_mom = g(g.exp_ixp(p) * src)
+
+        laplace = sum([2.0 * (np.cos(p[i]) - 1.0) for i in dimensions])
+        factor = (1.0 + kappa / (1 + 2 * len(dimensions) * kappa) * laplace ) ** steps
+        dst = g(smear_unit * src_mom)
+        eps2 = g.norm2(dst - factor * src_mom) / g.norm2(dst)
+        g.message(f"Wuppertal test using eigen representation: {eps2}")
+        assert eps2 < 1e-28
+
