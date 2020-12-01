034fbdc967df686460b268db0240096b10e41fbb
Christoph Lehner <christoph@lhnr.de>
2020-12-01 14:18:11 +0100

    towards a unified make script

--- scripts/source.sh
+++ scripts/source.sh
@@ -1,5 +1,5 @@
 #
 # Add lib to python path (without ":" if empty beforehand)
 #
-lib="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../lib" >/dev/null 2>&1 && pwd )"
+lib="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../lib/build" >/dev/null 2>&1 && pwd )"
 export PYTHONPATH=${lib}${PYTHONPATH:+:${PYTHONPATH}}
