d1b8e9f96586bfb3eaa7df5e07739510f18622e4
Christoph Lehner <christoph@lhnr.de>
2020-12-10 09:27:54 +0100

    gpu ternary compatibility

--- lib/cgpt/lib/foundation/ternary.h
+++ lib/cgpt/lib/foundation/ternary.h
@@ -20,6 +20,40 @@
 template<typename S, typename T>
 inline void cgpt_where(Lattice<T>& answer, const Lattice<S>& question, const Lattice<T>& yes, const Lattice<T>& no) {
 
-  answer = where( TensorRemove(question) == 0.0, no, yes);
+  GridBase* grid = answer.Grid();
+  conformable(grid, question.Grid());
+  conformable(grid, yes.Grid());
+  conformable(grid, no.Grid());
+
+  typedef typename Lattice<S>::scalar_object S_sobj;
+  typedef typename Lattice<S>::vector_object S_vobj;
+  typedef typename Lattice<T>::scalar_object T_sobj;
+  typedef typename Lattice<T>::vector_object T_vobj;
+  
+  autoView(answer_v, answer, CpuWriteDiscard);
+  autoView(question_v, question, CpuRead);
+  autoView(yes_v, yes, CpuRead);
+  autoView(no_v, no, CpuRead);
+
+  auto oSites = grid->oSites();
+  auto Nsimd = grid->Nsimd();
+
+  thread_for(i, oSites, {
+
+      ExtractBuffer<S_sobj> vquestion(Nsimd);
+      ExtractBuffer<T_sobj> vyes(Nsimd);
+      ExtractBuffer<T_sobj> vno(Nsimd);
+      ExtractBuffer<T_sobj> vanswer(Nsimd);
+
+      extract<S_vobj,S_sobj>(question_v[i],vquestion);
+      extract<T_vobj,T_sobj>(yes_v[i],vyes);
+      extract<T_vobj,T_sobj>(no_v[i],vno);
+
+      for(int s=0;s<Nsimd;s++){
+	vanswer[s] = (norm2(vquestion[s]) != 0.0) ? vyes[s] : vno[s];
+      }
+
+      merge<T_vobj,T_sobj>(answer_v[i],vanswer);
+    });
 
 }
