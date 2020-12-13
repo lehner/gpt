770c5454ff86723fc0d884fae8a09e23a467e38a
Christoph Lehner <christoph@lhnr.de>
2020-12-13 22:06:28 +0100

    remove deprecated scripts/source.sh

--- scripts/source.sh
+++
@@ -1,5 +0,0 @@
-#
-# Add lib to python path (without ":" if empty beforehand)
-#
-lib="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../lib/build" >/dev/null 2>&1 && pwd )"
-export PYTHONPATH=${lib}${PYTHONPATH:+:${PYTHONPATH}}
