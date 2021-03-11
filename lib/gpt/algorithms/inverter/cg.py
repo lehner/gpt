5f07a1b12838b87b6a175b9f30d0818083b9d791
Simon Bürger <simon.buerger@rwth-aachen.de>
2021-03-11 16:50:29 +0100

    add (optional) preconditioning to CG

--- lib/gpt/algorithms/inverter/cg.py
+++ lib/gpt/algorithms/inverter/cg.py
@@ -29,53 +29,70 @@ class cg(base_iterative):
         self.eps = params["eps"]
         self.maxiter = params["maxiter"]
 
-    def __call__(self, mat):
+    def __call__(self, mat, approx_inv_mat=None):
 
+        # remove wrapper for performance benefits
         otype, grid, cb = None, None, None
         if type(mat) == g.matrix_operator:
             otype, grid, cb = mat.otype, mat.grid, mat.cb
             mat = mat.mat
-            # remove wrapper for performance benefits
+        if type(approx_inv_mat) == g.matrix_operator:
+            approx_inv_mat = approx_inv_mat.mat
 
         @self.timed_function
         def inv(psi, src, t):
             assert src != psi
             t("setup")
             p, mmp, r = g.copy(src), g.copy(src), g.copy(src)
+            if approx_inv_mat is not None:
+                z = g.copy(src)
+            else:
+                z = r  # reference the same lattice object
             mat(mmp, psi)  # in, out
             d = g.inner_product(psi, mmp).real
             b = g.norm2(mmp)
             r @= src - mmp
-            p @= r
-            a = g.norm2(p)
-            cp = a
+            if approx_inv_mat is not None:
+                approx_inv_mat(z, r)
+            p @= z
+            cp = g.inner_product(r, z).real  # this is always real
             ssq = g.norm2(src)
             if ssq == 0.0:
-                assert a != 0.0  # need either source or psi to not be zero
-                ssq = a
+                assert cp != 0.0  # need either source or psi to not be zero
+                ssq = cp
             rsq = self.eps ** 2.0 * ssq
             for k in range(self.maxiter):
                 c = cp
+
                 t("matrix")
                 mat(mmp, p)
+
                 t("inner_product")
-                dc = g.inner_product(p, mmp)
-                d = dc.real
+                d = g.inner_product(p, mmp).real
                 a = c / d
+
                 t("axpy_norm2")
-                cp = g.axpy_norm2(r, -a, mmp, r)
+                norm_r = g.axpy_norm2(r, -a, mmp, r)
+                if approx_inv_mat is None:
+                    cp = norm_r
+                else:
+                    t("preconditioner")
+                    approx_inv_mat(z, r)
+                    cp = g.inner_product(r, z).real
+
                 t("linear combination")
                 b = cp / c
                 psi += a * p
-                p @= b * p + r
+                p @= b * p + z
+
                 t("other")
-                self.log_convergence(k, cp, rsq)
-                if cp <= rsq:
+                self.log_convergence(k, norm_r, rsq)
+                if norm_r <= rsq:
                     self.log(f"converged in {k+1} iterations")
                     return
 
             self.log(
-                f"NOT converged in {k+1} iterations;  squared residual {cp:e} / {rsq:e}"
+                f"NOT converged in {k+1} iterations;  squared residual {norm_r:e} / {rsq:e}"
             )
 
         return g.matrix_operator(
