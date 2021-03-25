13443aacb2e1a4255874d3a38fb9afc7aa8348f7
Daniel Richtmann <daniel.richtmann@gmail.com>
2021-03-25 14:33:11 +0100

    Check in missing coarse_multi_arg.h

---
+++ lib/cgpt/lib/operators/coarse_multi_arg.h
@@ -0,0 +1,50 @@
+/*
+    GPT - Grid Python Toolkit
+    Copyright (C) 2020  Christoph Lehner (christoph.lehner@ur.de, https://github.com/lehner/gpt)
+                  2020  Daniel Richtmann (daniel.richtmann@ur.de)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+template<typename vCoeff_t>
+cgpt_multi_arg_fermion_operator_base* cgpt_create_multi_arg_coarsenedmatrix(PyObject* args) {
+
+  auto grid_c = get_pointer<GridCartesian>(args,"U_grid");
+  auto grid_c_rb = get_pointer<GridRedBlackCartesian>(args,"U_grid_rb");
+  int make_hermitian = get_int(args,"make_hermitian");
+  int level = get_int(args,"level"); // 0 = fine, increases with coarser levels
+  int nbasis = get_int(args,"nbasis");
+  // std::vector<ComplexD> dag_factor = get_complex_vec_gen(args,"dag_factor");
+
+#define BASIS_SIZE(n) \
+  if(n == nbasis) { \
+    auto ASelfInv_ptr_list = get_pointer_vec<cgpt_Lattice_base>(args,"U_self_inv"); \
+    auto A_ptr_list = get_pointer_vec<cgpt_Lattice_base>(args,"U");     \
+    \
+    PVector<Lattice<iMSinglet ##n<vCoeff_t>>> ASelfInv; \
+    PVector<Lattice<iMSinglet ##n<vCoeff_t>>> A; \
+    \
+    cgpt_basis_fill(ASelfInv, ASelfInv_ptr_list); \
+    cgpt_basis_fill(A, A_ptr_list); \
+    \
+    ASSERT(A.size() == 9*ASelfInv.size()); \
+    \
+    typedef MultiArgVirtualCoarsenedMatrix<iSinglet<vCoeff_t>, n> CMat; \
+    auto cm = new CMat(A, ASelfInv, *grid_c, *grid_c_rb, make_hermitian, 1); \
+    return new cgpt_multi_arg_coarse_operator<CMat>(cm); \
+  } else
+#include "../basis_size.h"
+#undef BASIS_SIZE
+    { ERR("Unknown basis size %d", (int)nbasis); }
+}
