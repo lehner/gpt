840c2caab9b710b780c11b302066e02673c53f18
Christoph Lehner <christoph@lhnr.de>
2021-06-16 18:56:51 +0200

    style

--- tests/algorithms/integrators.py
+++ tests/algorithms/integrators.py
@@ -8,18 +8,21 @@ import gpt as g
 rng = g.random("test")
 grid = g.grid([4, 4, 4, 8], g.double)
 
+
 def dC_su_n(u):
     r = g(g.qcd.gauge.project.traceless_anti_hermitian(g(u * V * u * u * V)) * (1j))
     r.otype = u.otype.cartesian()
     return r
 
+
 def dC_u1(u):
     r = g(g.component.log(u) / 1j)
-    r @= r*r - r*r*r
+    r @= r * r - r * r * r
     r.otype = u.otype.cartesian()
     return r
 
-for group, dC in [(g.mcolor, dC_su_n), (g.u1,dC_u1)]:
+
+for group, dC in [(g.mcolor, dC_su_n), (g.u1, dC_u1)]:
     U = group(grid)
     V = group(grid)
     rng.element([U, V])
@@ -35,9 +38,11 @@ for group, dC in [(g.mcolor, dC_su_n), (g.u1,dC_u1)]:
     delta = eps / N_steps
     for i in range(N_steps):
         U_delta @= g.matrix.exp(1j * dC(U_delta) * delta) * U_delta
-        
-    eps_test = g.norm2(U_delta - U_eps)**0.5 / U_eps.grid.gsites / U_eps.otype.nfloats / eps
-    eps_ref = 10*delta**2.
+
+    eps_test = (
+        g.norm2(U_delta - U_eps) ** 0.5 / U_eps.grid.gsites / U_eps.otype.nfloats / eps
+    )
+    eps_ref = 10 * delta ** 2.0
     g.message(f"Test on {U.otype.__name__}: {eps_test} < {eps_ref}")
     assert eps_test < eps_ref
 
@@ -48,7 +53,7 @@ for group, dC in [(g.mcolor, dC_su_n), (g.u1,dC_u1)]:
 U = g.complex(grid)
 U[:] = 1.0
 eps = 0.01
-U_eps = g.algorithms.integrator.runge_kutta_4(U, lambda u: g(u*u), eps)[0,0,0,0]
+U_eps = g.algorithms.integrator.runge_kutta_4(U, lambda u: g(u * u), eps)[0, 0, 0, 0]
 U_exp = 1.0 / (1.0 - eps)
 eps_test = abs(U_eps - U_exp) / eps
 eps_ref = eps ** 3.0
