abfe089a44cf8c0825e3b9130e9c160d213908f6
Christoph Lehner <christoph@lhnr.de>
2021-06-20 09:46:47 +0200

    first demonstration of fourier accelerated coulomb gauge fixer

---
+++ applications/gauge_fix/coulomb.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import gpt as g
+
+U = g.load("/p/project/gm2dwf/configs/64I/ckpoint_lat.Coulomb.1200")
+
+# split in time
+Usep = [g.separate(u,3) for u in U[0:3]]
+Nt = U[0].grid.gdimensions[3]
+
+# gradient descent
+gd = g.algorithms.optimize.gradient_descent(maxiter=100000, eps=1e-9, step=0.1)
+
+# Coulomb functional on each time-slice
+for t in range(Nt):
+    Ut = [Usep[mu][t] for mu in range(3)]
+    V = g.identity(Ut[0])
+    g.random("f").element(V)
+    f, df = g.qcd.gauge.fix.landau(Ut)
+    fa_df = g.algorithms.optimize.fourier_accelerate.inverse_phat_square(V.grid, df)
+
+    gd(f, fa_df)(V)
+    
+    dfv = df(V)
+    theta = g.norm2(dfv) / V.grid.gsites / dfv.otype.Nc
+    g.message(t, theta)
+    g.message(V[0,0,0])
--- lib/gpt/algorithms/optimize/gradient_descent.py
+++ lib/gpt/algorithms/optimize/gradient_descent.py
@@ -42,7 +42,7 @@ class gradient_descent(base_iterative):
 
                 if i % self.nf == 0:
                     v = f(x)
-                    self.log(f"iteration {i}: f(x) = {v:e}")
+                    self.log(f"iteration {i}: f(x) = {v:e}, |df|/V = {rs:e}")
 
                 if rs <= self.eps:
                     self.log(f"converged in {i+1} iterations")
