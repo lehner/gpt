0e1558aeed8c23450315d84af999ecf133048e78
Mattia Bruno <38449948+mbruno46@users.noreply.github.com>
2022-05-25 15:33:35 +0200

    style

--- lib/gpt/core/grid.py
+++ lib/gpt/core/grid.py
@@ -96,9 +96,7 @@ def grid_get_mpi_default(fdimensions, cb):
 
 
 class grid:
-    def __init__(
-        self, fdimensions, precision, cb=None, obj=None, mpi=None, parent=None
-    ):
+    def __init__(self, fdimensions, precision, cb=None, obj=None, mpi=None, parent=None):
 
         self.fdimensions = fdimensions
         self.fsites = np.prod(self.fdimensions)
@@ -148,11 +146,7 @@ class grid:
         self,
     ):  # creates a string without spaces that can be used to construct it again, this should only describe the grid geometry not the mpi
         s = (
-            str(self.fdimensions)
-            + ";"
-            + self.precision.__name__
-            + ";"
-            + self.cb.__name__
+            str(self.fdimensions) + ";" + self.precision.__name__ + ";" + self.cb.__name__
         ).replace(" ", "")
         if self.parent is not None:
             s += ";" + self.parent.describe()
@@ -200,9 +194,7 @@ class grid:
         cb = general(
             self.cb.n,
             self.cb.cb_mask[0:dimension] + [cb_mask] + self.cb.cb_mask[dimension:],
-            self.cb.simd_mask[0:dimension]
-            + [simd_mask]
-            + self.cb.simd_mask[dimension:],
+            self.cb.simd_mask[0:dimension] + [simd_mask] + self.cb.simd_mask[dimension:],
         )
 
         if self.parent is None:
