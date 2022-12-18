f9076bcde0cdc19dea2da933d7968f0aa0e417f8
Christoph Lehner <christoph@lhnr.de>
2022-12-18 09:46:14 -0500

    checkpointed solver

--- lib/gpt/algorithms/inverter/__init__.py
+++ lib/gpt/algorithms/inverter/__init__.py
@@ -39,3 +39,4 @@ from gpt.algorithms.inverter.multi_shift_fom import multi_shift_fom
 from gpt.algorithms.inverter.multi_shift_fgmres import multi_shift_fgmres
 from gpt.algorithms.inverter.subspace_minimal_residual import subspace_minimal_residual
 from gpt.algorithms.inverter.solution_history import solution_history
+from gpt.algorithms.inverter.checkpointed import checkpointed
