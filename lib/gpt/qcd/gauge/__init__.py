d8ed37fa90454dd03f66111d99f6d273e1428540
Christoph Lehner <christoph@lhnr.de>
2024-06-29 18:26:44 +0200

    stencil version of field-strength topology

--- lib/gpt/qcd/gauge/__init__.py
+++ lib/gpt/qcd/gauge/__init__.py
@@ -18,10 +18,10 @@
 #
 from gpt.qcd.gauge.create import random, unit
 from gpt.qcd.gauge.loops import rectangle, field_strength
-from gpt.qcd.gauge.topology import topological_charge, topological_charge_5LI
+from gpt.qcd.gauge.topology import topological_charge_5LI
 from gpt.qcd.gauge.staples import staple
 from gpt.qcd.gauge.transformation import transformed
-from gpt.qcd.gauge.stencil import plaquette, staple_sum, energy_density, algebra_laplace
+from gpt.qcd.gauge.stencil import plaquette, staple_sum, energy_density, algebra_laplace, topological_charge
 import gpt.qcd.gauge.project
 import gpt.qcd.gauge.smear
 import gpt.qcd.gauge.fix
