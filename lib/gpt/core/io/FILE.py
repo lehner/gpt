8b0e9ec9973316cfc859e147f4982cde04ccc408
Christoph Lehner <christoph@lhnr.de>
2025-07-09 07:26:46 +0000

    file improvements

--- lib/gpt/core/io/FILE.py
+++ lib/gpt/core/io/FILE.py
@@ -16,7 +16,7 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-import cgpt, gpt, os, shutil, zipfile
+import cgpt, gpt, os, sys, shutil, zipfile
 
 
 def cache_file(root, src, md):
